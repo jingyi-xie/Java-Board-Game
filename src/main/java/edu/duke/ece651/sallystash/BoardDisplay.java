72306ee305c8c788ba1de00b4b9f88be5dbb2bbd
Jingyi Xie <jingyi.xie@duke.edu>
2020-02-11 22:37:28 -0500

    display two board

--- src/main/java/edu/duke/ece651/sallystash/BoardDisplay.java
+++ src/main/java/edu/duke/ece651/sallystash/BoardDisplay.java
@@ -1,28 +1,39 @@
 package edu.duke.ece651.sallystash;
 
 public class BoardDisplay {
-  private Board board;
-  public BoardDisplay(Board b) {
-      this.board = b;
+  final int AS_SELF = 0;
+  final int AS_OPP = 1;
+
+  private Board board_a;
+  private Board board_b;
+  public BoardDisplay() {
+    this.board_a = new Board();
+    this.board_b = new Board();
   }
-  public void refresh(Board b) {
-      this.board = b;
+  public void updateA(Board a) {
+    this.board_a = a;
+  }
+  public void updateB(Board b) {
+    this.board_b = b;
+  }
+
+  private void printNum() {
+    char[] col_index = {' ', ' ','0', '|', '1', '|', '2', '|', '3', '|', '4', '|', '5', '|', '6', '|', '7', '|', '8', '|', '9', ' ', ' '};
+    System.out.print(col_index);
+    System.out.print("                          ");
+    System.out.print(col_index);
+    System.out.println();
   }
   public void display() {
-    char[] col_index = {' ', ' ','0', '|', '1', '|', '2', '|', '3', '|', '4', '|', '5', '|', '6', '|', '7', '|', '8', '|', '9', ' ', ' '};
-    System.out.println(col_index);
+    printNum();
+    LineDisplay ld_a = new LineDisplay(board_a);
+    LineDisplay ld_b = new LineDisplay(board_b);
     for (int i = 0; i < 20; i++) {
-      int letter = (char)65 + i;
-      System.out.print((char)letter + " ");
-      for (int j =0; j < 10; j++) {
-        System.out.print(board.getCell(i, j).getColor());
-        if (j == 9) {
-            continue;
-        }
-        System.out.print("|");
-      }
-      System.out.println(" " + (char)letter);
+        ld_a.displayLine(AS_SELF, i);
+        System.out.print("                          ");
+        ld_b.displayLine(AS_OPP, i);
+        System.out.println();
     }
-    System.out.println(col_index);
+    printNum();
   }
 }
--- src/main/java/edu/duke/ece651/sallystash/Launcher.java
+++ src/main/java/edu/duke/ece651/sallystash/Launcher.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Launcher {
   public static void main(String[] args) {
     Board brd = new Board();
-    BoardDisplay bd = new BoardDisplay(brd);
+    BoardDisplay bd = new BoardDisplay();
     bd.display();
     Scanner scan_i = new Scanner(System.in);
     System.out.println("Enter i");
@@ -13,9 +13,9 @@ public class Launcher {
     Scanner scan_j = new Scanner(System.in);
     System.out.println("Enter j");
     int j = scan_j.nextInt();
-    Rectangle rec = new Rectangle(1, 'g', 1, 2);
+    Rectangle rec = new Rectangle(1, 'G', 1, 2);
     rec.putOnBoard(i, j, brd);
-    bd.refresh(brd);
+    bd.updateA(brd);
     bd.display();
     scan_i.close();
     scan_j.close();
